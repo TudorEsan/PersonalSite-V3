f0f3b0f60d1b6043f76b0d421be67d2225049c85
Tudor <tudor.esan@icloud.com>
2022-12-10 02:30:58 +0200

    responsive hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { BasicButton } from "../components/Buttons/BasicButton";
 
 export default function Home() {
   return (
@@ -7,18 +8,22 @@ export default function Home() {
         <Image
           className="flex-[2] max-h-80"
           src="/Me.png"
+          style={{
+            objectFit: "contain",
+          }}
           quality={100}
           height={500}
           width={500}
           alt="An image with me!"
         />
         {/* image with max width */}
-        <div className="min-w-full text-center font-semibold m-auto flex-[3] sm:text-3xl md:text-6xl sm:text-center">
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 via-purple-600 to-blue-600">
+        <div className="text-xl sm:text-3xl md:text-6xl sm:text-left min-w-12 text-center font-semibold m-auto flex-[3]">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-amber-400 via-purple-600 to-blue-600">
             Hello world!
           </span>
           <br />
           Welcome to my website!
+          <BasicButton className="pt-2">Contact Me!</BasicButton>
         </div>
       </div>
     </div>
